5d1f581796dcdb6e25f08a42b422c8adf004ebf7
Abdul Wahab Khan <abdulwahab052001@gmail.com>
2022-09-24 20:51:07 +0500

    Update classwork.sql

--- Labs/DB Lab 5/classwork.sql
+++ Labs/DB Lab 5/classwork.sql
@@ -28,74 +28,121 @@ from employees e, departments d;
 
 
 
+select first_name from employees where department_id IN ( select department_id from departments);
+
+select first_name from employees natural join departments;
+
 --Task1
-select e.first_name || ' ' || e.last_name as Fullname, d.department_name  
-from EMPLOYEES e, DEPARTMENTS d 
-where e.manager_id=d.manager_id;
+select d.department_name, e.first_name || '' || e.last_name as Fullname, l.city
+from employees e inner join departments d 
+on e.manager_id = d.manager_id
+inner join locations l
+on d.location_id=l.location_id;
 
---Task2
-select e.employee_id, e.job_id, to_date(sysdate,'DD-MM-YYYY')-to_date(e.hire_date,'DD-MM_YYYY') noofdaysworked from employees e where e.department_id=80;
+--task2
+select to_date(sysdate,'DD-MM-YYYY')-to_date(e.hire_date,'DD-MM-YYYY') as noofdaysworked, e.employee_id, e.job_id
+from employees e inner join departments d
+on e.department_id=d.department_id
+where d.department_id=80;
 
---Task3
-select e.first_name || ' ' || e.last_name as Fullname,
-e.job_id as Jobtitle,j.start_date,j.end_date
-from employees e, job_history j 
+--task3
+select e.first_name || '' || e.last_name as Fullname, e.job_id, j.start_date, j.end_date
+from employees e right outer join job_history j 
+on e.employee_id=j.employee_id
 where e.commission_pct is null;
 
---Task4
-select d.department_name, d.department_id, count(*) as employees
-from departments d join employees e
-on d.department_id=e.department_id
-group by d.department_name, d.department_id; 
+--task4
+select d.department_name, d.department_id, count(*) 
+from employees e inner join departments d 
+on e.department_id=d.department_id
+group by d.DEPARTMENT_ID, d.DEPARTMENT_NAME;
 
 --task5
-select E.FIRST_NAME || E.LAST_NAME as Name , E.SALARY  from Employees E
-inner join DEPARTMENTS D
-on D.DEPARTMENT_ID=E.DEPARTMENT_ID
-inner join Locations L
-on L.LOCATION_ID=D.LOCATION_ID
-where L.CITY= 'London';
+select e.first_name || '' || e.last_name as Fullname, e.salary 
+from employees e inner join departments d
+on e.department_id=d.department_id
+inner join locations l
+on l.location_id=d.location_id
+where l.city='London';
 
---TASK 6:
-select distinct E.FIRST_NAME || E.LAST_NAME as Name   from Employees E
-left join JOB_HISTORY J on J.EMPLOYEE_ID=E.EMPLOYEE_ID
-where J.EMPLOYEE_ID is NULL;
+--task6
+select e.first_name || '' || e.last_name as Fullname
+from employees e inner join job_history j
+on e.employee_id=j.employee_id
+where j.employee_id is null;
 
+--task7
+select c.country_name
+from countries c inner join regions r
+on c.region_id=r.region_id
+where r.region_name='Asia';
 
---TASK 7:
-select * from COUNTRIES C
-inner join REGIONS R on C.REGION_ID=R.REGION_ID
-where R.REGION_NAME = 'Asia';
+--task8
+select e.first_name || '' || e.last_name as Fullname, j.job_title as Jobname, d.department_name, e.salary
+from employees e inner join departments d
+on e.department_id=d.department_id
+inner join jobs j
+on j.job_id=e.job_id
+order by d.DEPARTMENT_NAME asc;
 
+--task9
+select d.department_name
+from employees e inner join departments d 
+on e.department_id=d.department_id
+group by d.DEPARTMENT_NAME
+having (count(*)>=2);
 
---TASK 8:
-select  E.FIRST_NAME || E.LAST_NAME as Name, D.DEPARTMENT_NAME, E.SALARY from Employees E
-inner join DEPARTMENTS D on D.DEPARTMENT_ID=E.DEPARTMENT_ID
-order by D.DEPARTMENT_NAME asc;
-
-
-
---TASK 9:
-select  D.DEPARTMENT_NAME from Employees E
-inner join Departments D on D.Department_ID=E.DEPARTMENT_ID
-group by D.DEPARTMENT_NAME
-Having (count (E.EMPLOYEE_ID) >=2);
-
-
---TASK 10:
-Select * from Employees E
-inner join JOBS J on E.JOB_ID=J.JOB_ID
-where E.SALARY < J.MIN_SALARY;
+--task10
+select *
+from employees e inner join jobs j
+on j.job_id=e.job_id
+where e.salary<j.min_salary;
 
 --task11
-select e.first_name || ' ' || e.last_name as Fullname, e.job_id as Jobname, e.salary*12 as annualsalary, e.department_id, l.city  
-from employees e join location l
-on 
+select e.first_name || '' || e.last_name as Fullname, j.job_title as jobname, e.salary*12, d.department_id, d.department_name, l.city
+from employees e inner join jobs j 
+on e.job_id=j.job_id
+inner join departments d
+on e.department_id=d.department_id
+inner join locations l
+on l.location_id=d.location_id
+where e.salary*12 > 60000 and j.job_title <> 'Analyst';
 
+--task12
+select *
+from employees e1 join employees e2
+on e1.employee_id=e2.manager_id;
 
---Task12
-select * from employees e join employees m
-on e.employee_id=m.manager_id;
+--Task13
+select  d.department_id,d.department_name
+from departments d left outer join employees e
+on d.department_id=e.department_id
+group by d.department_id,d.department_name 
+having count(d.department_id) = 0;
 
---task13
-select 
+--task14
+select e.first_name || '' || e.last_name as Fullname,e.salary, d.department_name
+from employees e left outer join departments d 
+on e.department_id=d.department_id;
+
+--task15
+select e.first_name || '' || e.last_name as Fullname, e.job_id, d.department_name
+from employees e inner join departments d
+on e.department_id=d.department_id
+inner join locations l
+on d.location_id=l.location_id
+where l.state_province is null;
+
+--task16
+select e.department_id
+from employees e left join departments d
+on e.department_id=d.department_id 
+where d.department_id is null;
+
+--task17
+select *
+from employees e inner join departments d
+on e.department_id=d.department_id
+inner join locations l 
+on d.location_id=l.location_id
+where l.country_id='US' and l.state_province <> 'Washington';
